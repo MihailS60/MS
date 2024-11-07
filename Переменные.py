751409d2ae7f5f0f4b5ea58dcc6eae73603268f2
MihailS60 <mstroganov0645@gmail.com>
2024-11-07 20:24:22 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,8 @@
+print("Тема Переменные")
+My_name='Mihail'
+print('Имя: '+My_name)
+Tasks_count=12
+Time_spent=1.5
+Subject='Python'
+Time_for_one_task=(Time_spent/float(Tasks_count))
+print('Курс:',Subject, ' всего задач:',Tasks_count, ',затрачено часов:',Time_spent, 'среднее время выполнения:',Time_for_one_task,'часа.')
